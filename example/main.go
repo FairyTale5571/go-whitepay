52e51d77dda11901491a45f889c812e6a57c4083
Oleksandr Onopchenko <a3atlaslife@gmail.com>
2024-03-26 21:18:32 +0200

    fix: go mod

--- example/main.go
+++ example/main.go
@@ -1,4 +1,24 @@
 package main
 
+import (
+	"context"
+	"log"
+	
+	"github.com/fairytale5571/go-whitepay"
+)
+
 func main() {
+	wp := whitepay.New("123", "slug")
+	ctx := context.Background()
+	var response, err = wp.CreateNewOrder(ctx, &whitepay.CreateNewOrderRequest{
+		Amount:          "10000",
+		Currency:        "USD",
+		ExternalOrderID: "123456",
+		SuccessfulLink:  "https://google.com",
+		FailureLink:     "https://bing.com",
+	})
+	if err != nil {
+		panic(err)
+	}
+	log.Printf("Response: %+v", response)
 }
--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module github.com/fairytale5571/go-whitepay
+
+go 1.22.1
